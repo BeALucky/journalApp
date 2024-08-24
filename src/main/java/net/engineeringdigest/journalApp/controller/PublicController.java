c8464d9a3b2e368bd6861ef99b7dff2d3e067858
BeALucky <bealucky.dev@gmail.com>
2024-08-25 00:50:47 +0530

    add health logerr

--- src/main/java/net/engineeringdigest/journalApp/controller/PublicController.java
+++ src/main/java/net/engineeringdigest/journalApp/controller/PublicController.java
@@ -1,5 +1,6 @@
 package net.engineeringdigest.journalApp.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import net.engineeringdigest.journalApp.entity.User;
 import net.engineeringdigest.journalApp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/public")
+@Slf4j
 public class PublicController {
 
     @Autowired
@@ -19,6 +21,8 @@ public class PublicController {
 
     @GetMapping("/health-check")
      public String healthCheck(){
-         return "OK working fine";
+         log.info("Health is OK");
+
+        return "OK working fine";
      }
 }
